332a5b6bb51d1805a70b8f3460d33487a44b3ada
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2024-09-29 18:58:53 -0400

    initial configurations

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Bieenvenido a la aplicación ToDo con Laravel y Vue.js</h1>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+
+  <style scoped>
+  /* Añade tus estilos aquí */
+  </style>
